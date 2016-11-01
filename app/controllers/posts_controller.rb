43120daabae17034f328ba49fc30c071c4f40ed5
Ryan Waits <ryan.waits@gmail.com>
2016-10-31 19:06:45 -0500

    add posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+  end
+
+  def show
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
