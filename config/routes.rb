aa1c233c3f942b04b5e3283cbaefb60fe7dd7270
Ryan Waits <ryan.waits@gmail.com>
2016-11-01 13:04:12 -0500

    add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'log_in' => 'sessions#new', as: 'login'
+  get 'log_out' => 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   
   resources :posts
+  resources :sessions, only: [:new, :create, :destroy]
 end
